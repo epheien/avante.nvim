07266c0142d4a0e4d5bb8d61d3874f9998ef507e
eph <eph@MacBook-Pro.local>
2026-01-04 22:16:46 +0800

    ...

--- lua/avante/providers/openai.lua
+++ lua/avante/providers/openai.lua
@@ -309,6 +309,7 @@ function M:finish_pending_messages(ctx, opts)
   if ctx.content ~= nil and ctx.content ~= "" then self:add_text_message(ctx, "", "generated", opts) end
   if ctx.tool_use_map then
     for _, tool_use in pairs(ctx.tool_use_map) do
+      -- NOTE: tool_use.state is always nil
       if tool_use.state == "generating" then self:add_tool_use_message(ctx, tool_use, "generated", opts) end
     end
   end
